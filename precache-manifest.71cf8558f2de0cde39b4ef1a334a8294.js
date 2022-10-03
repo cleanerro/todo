54616d344aa58646ff68704302ed2be4c3cf0ea5
cleanerro <romastepanyuk@gmail.com>
2022-10-04 02:45:41 +0300

    Updates

---
+++ precache-manifest.71cf8558f2de0cde39b4ef1a334a8294.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d7d93044a2e433b8e4b1fed304f9b6",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "1688a140569ae0437629",
+    "url": "/todo/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "10335fa8e16e252a7a9a",
+    "url": "/todo/static/js/2.6e6559c3.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todo/static/js/2.6e6559c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1688a140569ae0437629",
+    "url": "/todo/static/js/main.7ec67abf.chunk.js"
+  },
+  {
+    "revision": "e9b3b084cc17457a6264",
+    "url": "/todo/static/js/runtime-main.c58ac565.js"
+  }
+]);
